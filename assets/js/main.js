303df5ab33fc08ed0f830cbca34dc3355b16acbb
Patricia Rocha Machado <patriciarocha0122@gmail.com>
2019-04-22 14:31:24 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,6 +47,25 @@ function sortear(){
 }
 }
 
+function calculadora() {    
+    var n1 = parseFloat(document.getElementById('n1').value);    
+    var n2 = parseFloat(document.getElementById('n2').value);    
+    var resultado = 0;  
+    if (document.getElementsByName('operacao')[0].checked) {    
+        resultado = n1 + n2;  
+    }    
+    if (document.getElementsByName('operacao')[1].checked) {    
+        resultado = n1 - n2;    
+    }    
+    if (document.getElementsByName('operacao')[2].checked) {    
+        resultado = n1 * n2;    
+    }    
+    if (document.getElementsByName('operacao')[3].checked) {    
+        resultado = n1 / n2;    
+    }    
+    document.getElementById('resultado').value = resultado;    
+ }  
+
 async function verificar(){
 
     await sleep(200);
@@ -103,6 +122,9 @@ async function verificar(){
 				);
 			});
 
-	});
+    });
+    
+
+   
 
 })(jQuery);
